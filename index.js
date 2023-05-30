efe2274ee18d28b9d2b9e3c818994f729d17831d
phuc2807 <128949868+phuc2807@users.noreply.github.com>
2023-05-30 21:33:30 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const wrapper = document.querySelector(".auth-section");
 const loginBtn = document.querySelector(".login");
+const closeBtn = document.querySelector("login-modal__close");
+const signin = document.querySelector(".sign-in-wrapper");
+const signup = document.querySelector(".sign-up-wrapper");
 
 loginBtn.addEventListener("click", () => {
   wrapper.classList.add("active");
 });
 
-wrapper.addEventListener("click", () => {
+closeBtn.addEventListener("click", () => {
   wrapper.classList.remove("active");
 });
+
+signin.addEventListener('click', () => {
+  document.getElementsByClassName("sign-in-wrapper").style.display = 'none';
+  document.getElementsByClassName("sign-up-wrapper").style.display = 'block';
+}); 
